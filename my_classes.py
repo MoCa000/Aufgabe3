30c5658ce94481eae722bbfed974a9404a3916bf
MoCa000 <mth.cassan@mci4me.at>
2025-04-08 11:32:48 +0200

    Aufgabe 6.2

--- my_classes.py
+++ my_classes.py
@@ -1,11 +1,22 @@
-class Subject():
+from datetime import datetime
 
-    def __init__(self, first_name, last_name, sex, age_years):
+class Person():
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+
+class Subject(Person):
+
+    def __init__(self, first_name, last_name, sex, geburtsdatum):
         self.sex = sex
-        self.age_years = age_years
+        #self.age_years = age_years
+        self.__geburtsdatum = datetime.strptime(geburtsdatum, "%Y-%m-%d")
+        #self.__geburtsdatum = geburtsdatum
         self.max_hr_bpm = None
+        heute = datetime.today()
+        #self.age_years = heute - self.__geburtsdatum
+        age_days = (heute - self.__geburtsdatum).days
+        self.age_years = age_days / 365.25
 
     def estimate_max_hr(self, age_years : int , sex : str) -> int:
         if sex == "male":
@@ -16,13 +27,13 @@ class Subject():
             # der input() öffnet ein Eingabefenster für den Nutzer und speichert die Eingabe
             max_hr_bpm  = input("Enter maximum heart rate:")
         self.max_hr_bpm = int(max_hr_bpm)
+        return max_hr_bpm, age_years
 
 
-class Supervisor():
+class Supervisor(Person):
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-
 
 class Experiment():
     def __init__(self, name, date):
@@ -34,8 +45,9 @@ class Experiment():
 
     def add_supervisor(self, supervisor):
         self.supervisor = supervisor
-
+"""
     def __str__(self):
         return f"Experiment: {self.name}, Date: {self.date}, Supervisor: {self.supervisor.first_name} {self.supervisor.last_name}, Subject: {self.subject.first_name} {self.subject.last_name}, Max HR: {self.subject.max_hr_bpm} bpm"
     def __repr__(self):
-        return f"Experiment({self.name}, {self.date}, {self.supervisor}, {self.subject})"
+        return f"Experiment({self.name}, {self.date}, {self.supervisor}, {self.subject})""
+"""
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-from my_classes import Subject, Supervisor, Experiment
+from my_classes import Subject, Supervisor, Experiment, Person
+from datetime import datetime
 
 if __name__ == "__main__":
     # Erstellen eines Leistungstests
     supervisor = Supervisor("FirstName", "LastName")
-    subject = Subject("FirstName", "LastName", "female", 30)
+    subject = Subject("FirstName", "LastName", "female", "2000-01-01")
     subject.estimate_max_hr(subject.age_years, subject.sex)
 
     experiment = Experiment("Leistungstest", "2021-01-01")
@@ -16,6 +17,6 @@ if __name__ == "__main__":
     print(experiment.__dict__) 
     print(supervisor.__dict__)
     print(subject.__dict__)
-    #print(subject.estimate_max_hr)
-    #print(subject.max_hr_bpm)
-    #Pandas Installiert
+    print()
+    print(subject.age_years)
+    print(subject.max_hr_bpm)
